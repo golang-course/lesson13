7a51edde83f2cf94d56a1987d83130ecc2a0d716
Dmitry <timbirlake@icloud.com>
2026-04-26 12:44:21 +0300

    MapInterface

--- IsEven_test.go
+++ IsEven_test.go
@@ -17,3 +17,19 @@ func BenchmarkInserXInMap100(b *testing.B) {
 		InsertXInMap(100, b)
 	}
 }
+
+func BenchmarkInserXInMapInterface100000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		InsertXInMapInterface(100000, b)
+	}
+}
+func BenchmarkInserXInMapInterface1000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		InsertXInMapInterface(1000, b)
+	}
+}
+func BenchmarkInserXInMapInterface100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		InsertXInMapInterface(100, b)
+	}
+}
--- main.go
+++ main.go
@@ -9,3 +9,10 @@ func InsertXInMap(x int, b *testing.B) {
 		testMap[i] = i
 	}
 }
+func InsertXInMapInterface(x int, b *testing.B) {
+	testMap := make(map[any]int, 0)
+	b.ResetTimer()
+	for i := 0; i < x; i++ {
+		testMap[i] = i
+	}
+}
