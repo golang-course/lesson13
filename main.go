9fb98648b42953cd1d1b3c2bb6ba5b12e8f96f9a
Dmitry <timbirlake@icloud.com>
2026-04-26 13:10:13 +0300

    Map VS Slice Insert

--- main.go
+++ main.go
@@ -17,3 +17,32 @@ func InsertXInSlice(x int, b *testing.B) {
 		testSlice = append(testSlice, i)
 	}
 }
+
+func SelectXFromMap(x int, b *testing.B) {
+	testMap := make(map[int]int, x)
+	for i := 0; i < x; i++ {
+		testMap[i] = i
+	}
+	var h int
+	b.ResetTimer()
+	for i := 0; i < x; i++ {
+		h = testMap[i]
+	}
+	if h != 0 { //just to pass unused h
+	}
+}
+
+func SelectXFromSlice(x int, b *testing.B) {
+	testSlice := make([]int, x)
+	for i := 0; i < x; i++ {
+		testSlice = append(testSlice, i)
+	}
+	var h int
+	b.ResetTimer()
+	for i := 0; i < x; i++ {
+		h = testSlice[i]
+	}
+	if h != 0 { //just to pass unused h
+	}
+
+}
